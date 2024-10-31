36d7a5edd1771730c9835087c528f0bc059d25de
RealLowlyBlug <prikoldes228@mail.ru>
2024-11-01 00:09:18 +0300

    Add files via upload

---
+++ permennie.py
@@ -0,0 +1,16 @@
+home_tasks = 12
+hours = 1.5
+name_ph = 'Python'
+time_for_one_task = hours/home_tasks
+print (
+    'Курс:', name_ph,",",
+       'всего задач:', home_tasks ,",",
+       'затрачено часов:',hours ,",",
+       'среднее время выполнения:',time_for_one_task ,
+       ' часа',
+       ".",
+       sep=''
+       )
+
+
+
